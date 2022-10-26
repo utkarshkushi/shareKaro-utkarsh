28da78135465ac25ada280267234efdd25dc6dea
utkarshkushi <utkarshkushi2002@gmail.com>
2022-10-27 02:27:19 +0530

    adding cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const showRouter = require('./routes/show');
 const downloadRouter = require('./routes/download');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+const cors =require('cors');
 
 
 const app = express();
@@ -11,6 +12,16 @@ const app = express();
 const connectDB = require('./config/bd');
 
 connectDB();
+
+//cors options
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
+
+
 app.use(express.static('public'));
 app.use(express.json());
 
